a4c3e06b6ea5800daa6f22d55cd7fb4748c28b6c
Can Ersoy <can.ersoy@knauf.com>
2025-05-06 20:33:21 +0200

    first version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,71 @@
+import { env } from "cloudflare:workers";
 import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+const kv = env.KV;
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+app.post("/generate", async (c) => {
+  const host = c.req.header("hx-current-url")
+  const url = (await c.req.formData()).get("url")?.toString();
+  if (!url) {
+    return c.text("URL is required", 400);
+  }
+  
+  const keys = await kv.list();
+  const allValues = keys.keys.map((key) => key.name);
+  for (const key of allValues){
+    const value = await kv.get(key);
+    if (value === url) {
+      console.log("URL already exists");
+      return c.text(`${host}${key}`);
+    }
+  }
+
+  const id = crypto.randomUUID().toString().substring(0, 5);
+
+  if(allValues.some((element) => element === id)){ {
+    const value = await kv.get(id);
+    if (value === url) {
+      return c.text(`${host}${id}`);
+    }
+    else{
+      console.log("ID already exists");
+      const newId = crypto.randomUUID().toString().substring(0, 5);
+      await kv.put(newId, url);
+      return c.text(`${host}${newId}`);
+    }
+  }}
+
+  await kv.put(id, url);
+
+  return c.text(`${host}${id}`);
+});
+
+
+app.get("/:id", async (c) => {
+  const id = c.req.param('id');
+  if (!id) {
+    return c.text("ID is required", 400);
+  }
+
+  console.log(id);
+
+  const keys = await kv.list();
+  console.log(keys);
+  const allValues = keys.keys.map((key) => key.name);
+  console.log(allValues);
+
+  if (!allValues.includes(id)) {
+    return c.text("URL not found", 404);
+  }
+  const url = await kv.get(id);
+  console.log(url);
+
+  if (!url) {
+    return c.text("URL not found", 404);
+  }
+
+  return c.redirect(url!);
 });
 
 export default app;
